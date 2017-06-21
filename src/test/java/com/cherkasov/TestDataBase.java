a5c2c4f257fe67cd79bfabbf0a558775730dddf4
iridiumhawk <hawk.tmn@gmail.com>
2017-06-21 18:19:18 +0300

    test database timeout

--- src/test/java/com/cherkasov/TestDataBase.java
+++ src/test/java/com/cherkasov/TestDataBase.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
+import java.sql.SQLException;
+import java.sql.Statement;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -36,5 +38,54 @@ public class TestDataBase {
   public void testSelectFromDb() throws Exception{
     assertEquals("Felix", hsqlDb.getColumnFromTable("star", "firstname").get(0));
   }
+
+  public void shouldReturnEmptyResultIfQueryTimeout() {
+//    SqlDataGetter sql = new SqlDataGetter(hsqlDb.getConn());
+    String query =
+        "CREATE PROCEDURE sleep(t BIGINT) "
+            + "NO SQL "
+            + "LANGUAGE JAVA PARAMETER STYLE JAVA "
+            + "EXTERNAL NAME 'CLASSPATH:java.lang.Thread.sleep';   ";
+
+//    ExecutionTimeMeter.startTimer("sleep_query");
+
+//    exception.expect(SQLException.class);
+    try (Statement st = hsqlDb.getConn().createStatement()) {
+
+      int timeOut = 2000;
+      Runnable runner = () -> {
+        try {
+          Thread.sleep(timeOut);
+          System.out.println("test");
+
+          st.cancel();
+//          hsqlDb.getConn().close();
+
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+      };
+
+      new Thread(runner).start();
+
+
+      st.execute(query);
+//      st.setQueryTimeout(2);
+//      System.out.println(st.getQueryTimeout());
+      st.executeQuery("CALL sleep(10000)");
+
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+/*    String sql = "select slow_query(10) from dual";
+    PreparedStatement stmt = conn.prepareStatement(sql);
+    stmt.setQueryTimeout(2);*/
+
+//    List<String> result = sql.getResultsFromTable(query, conf.getValue("csv.delimiter"));
+//    ExecutionTimeMeter.stopTimer("sleep_query");
 
 }
